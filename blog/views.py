48f701030dc5e32c74d3ed27e27888a0698787f5
thomas <tomapi.tomato@gmail.com>
2026-05-24 17:35:47 +0200

    Added comment system

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import CommentForm
 
 def post_list(request):
     posts_reversed = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = posts_reversed.reverse()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
